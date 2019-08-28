df76bb28c29fdb580b069103816094d5b0bb2567
Sparkenstein <zetabytes.pp@gmail.com>
2019-08-28 22:17:33 +0530

    basic clap cli structure created

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+extern crate clap;
+
+use clap::{App, Arg};
+
 fn main() {
-	println!("Hello, world!");
+    let matches = App::new("Pack")
+        .version("1.0")
+        .author("Muthu Kumar")
+        .about(" pack and move at a moment's notice")
+        .arg(
+            Arg::with_name("pack")
+                .short("p")
+                .long("pack")
+                .help("Generate Pack manifest"),
+        )
+        .arg(
+            Arg::with_name("add")
+                .short("a")
+                .long("add")
+                .help("Add current path to Pack manifest"),
+        )
+        .arg(
+            Arg::with_name("up")
+                .short("u")
+                .long("up")
+                .help("packages all the chosen paths and creates an archive"),
+        )
+        .arg(
+            Arg::with_name("push")
+                .short("z")
+                .long("push")
+                .help("Push generated archive to location"),
+        )
+        .get_matches();
+
+	println!("{:?}", matches.args);
 }
