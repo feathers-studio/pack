a0c7bc8402497793ac53651eb04e9f70e132570a
Sparkenstein <zetabytes.pp@gmail.com>
2019-09-08 19:53:44 +0530

    getting manifest path now

--- src/subcommands/start.rs
+++ src/subcommands/start.rs
@@ -1,7 +1,19 @@
 use clap::{ArgMatches, SubCommand};
+use dirs::home_dir;
+use std::path::PathBuf;
 
 pub fn pack(c: SubCommand) {
-	let args: ArgMatches = c.matches;
-	// args 
-	println!("Start {:?}", args);
+	let _args: ArgMatches = c.matches;
+	let filename = format!(".{}_manifest", env!("CARGO_PKG_NAME"));
+	if let Some(dir) = home_dir() {
+		let manifest = get_manifest(dir, filename);
+		println!("{:?}", manifest);
+	}
+}
+
+fn get_manifest(dir: PathBuf, filename: String) -> PathBuf {
+	let mut manifest_path = PathBuf::new();
+	manifest_path.push(dir);
+	manifest_path.push(filename);
+	manifest_path
 }
