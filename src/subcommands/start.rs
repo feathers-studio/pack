9900f828b260e5444ca11d8d3dc09f7ea4118885
Sparkenstein <zetabytes.pp@gmail.com>
2019-09-08 20:10:46 +0530

    badly written file reading working somehow

--- src/subcommands/start.rs
+++ src/subcommands/start.rs
@@ -1,13 +1,24 @@
 use clap::{ArgMatches, SubCommand};
 use dirs::home_dir;
 use std::path::PathBuf;
+use std::fs::File;
+use std::io::Read;
 
 pub fn pack(c: SubCommand) {
 	let _args: ArgMatches = c.matches;
 	let filename = format!(".{}_manifest", env!("CARGO_PKG_NAME"));
 	if let Some(dir) = home_dir() {
 		let manifest = get_manifest(dir, filename);
-		println!("{:?}", manifest);
+		if !manifest.exists() {
+			let _file = File::create(manifest.as_path());
+		} else {
+			let res = File::open(manifest.as_path());
+			let mut file = res.unwrap();
+			let mut content = String::new();
+			file.read_to_string(&mut content).unwrap();
+			println!("file content {}", content);
+		}
+
 	}
 }
 
