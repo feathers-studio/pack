e71c91a61c7d406e9fa57c4d9dc02abff7a4ddca
Muthu Kumar <muthukumar@thefeathers.in>
2022-10-20 17:28:24 +0530

    here we go again

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # pack
 
 pack -- pack and move at a moment's notice.
-
-## Contributors
-
-- [MKRhere](https://github.com/MKRhere) Muthu Kumar - creator, maintainer
--- src/classifier.rs
+++
@@ -1,9 +0,0 @@
-use crate::subcommands;
-use clap::SubCommand;
-
-pub fn init(c: SubCommand) {
-	match c.name.as_ref() {
-		"start" => subcommands::start::pack(c),
-		_ => println!("Command not found"),
-	}
-}
