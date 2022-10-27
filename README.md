79f8c9039661bfec23cbe7012e300c7cfecf3096
Muthu Kumar <muthukumar@thefeathers.in>
2022-10-27 21:50:33 +0530

    feat: implement pack init, add

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # pack
 
 pack -- pack and move at a moment's notice.
+
+## Building
+
+Use zig master. Just run
+
+```shell
+zig build
+```
+
+## Usage
+
+Only some commands are implemented so far.
+
+### pack init
+
+Creates an empty `.pack` file in cwd if it doesn't exist yet.
+
+### pack add [...files]
+
+Adds given file paths to `.pack` file.
+
+### pack pack (not implemented)
+
+Pulls the files specified in `.pack` to current directory, preserving their paths and creates an `unpack` binary next to them. This repo can be committed to git.
+
+### ./unpack (not implemented)
+
+Since pack creates an unpack file, target machine doesn't need `pack` to unpack a pack-ed archive.
--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,107 @@
 const std = @import("std");
+const eql = std.mem.eql;
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+fn readConfig(arraylist: *std.ArrayList([]u8), allocator: std.mem.Allocator) !void {
+    const file = try std.fs.cwd().openFile(".pack", .{});
+    defer file.close();
+    const reader = file.reader();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+    // reading file into arraylist sep by \n
+    while (try reader.readUntilDelimiterOrEofAlloc(allocator, '\n', 4096)) |line| {
+        try arraylist.append(line);
+    }
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn equal(a: []const u8, b: []const u8) bool {
+    return eql(u8, a, b);
+}
+
+const help =
+    \\Help text
+    \\
+;
+
+pub fn main() !u8 {
+    // const log = std.io.getStdOut().writer().print;
+    // const err = std.io.getStdErr().writer().print;
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(!gpa.deinit());
+    const allocator = gpa.allocator();
+
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    if (args.len < 2) {
+        try std.io.getStdErr().writer().print("No command passed.\n\n" ++ help, .{});
+        return 1;
+    }
+
+    const command = args[1];
+
+    // -- pack help
+    if (equal("help", command)) {
+        std.debug.print(help, .{});
+        return 0;
+    }
+
+    // -- pack init
+    if (equal("init", command)) {
+        const file = std.fs.cwd().createFile(".pack", .{ .exclusive = true }) catch |err| {
+            switch (err) {
+                error.PathAlreadyExists => {
+                    std.debug.print(".pack file already exists in current directory. Skipping.\n", .{});
+                    return 0;
+                },
+                else => {
+                    std.debug.print("Surprising.\n", .{});
+                    return err;
+                },
+            }
+        };
+        defer file.close();
+        std.debug.print("Created empty .pack file in current directory.\n", .{});
+        return 0;
+    }
+
+    const file = try std.fs.cwd().openFile(".pack", .{ .mode = .read_write });
+    defer file.close();
+
+    // -- pack add
+    if (equal("add", command)) {
+        if (args.len < 3) {
+            try std.io.getStdErr().writer().print("Pass at least one file to add.\n", .{});
+            return 2;
+        }
+        const files = args[2..];
+        for (files) |f| {
+            try file.seekFromEnd(0);
+            try file.writeAll(f);
+            try file.writeAll("\n");
+        }
+
+        return 0;
+    }
+
+    // -- pack pack
+    if (equal("pack", command)) {
+        // try to read config
+        const reader = file.reader();
+        var configList = std.ArrayList([]u8).init(allocator);
+        defer configList.deinit();
+        defer for (configList.items) |line| allocator.free(line);
+
+        // reading file into arraylist sep by \n
+        while (try reader.readUntilDelimiterOrEofAlloc(allocator, '\n', 4096)) |line| {
+            try configList.append(line);
+        }
+
+        std.debug.print("\nPacking:\n\n", .{});
+        for (configList.items) |line| std.debug.print(" {s}\n", .{line});
+        std.debug.print("\nDone!\n\n", .{});
+        return 0;
+    }
+
+    std.debug.print("Unknown command: {s}\n\n" ++ help, .{command});
+    return 2;
 }
