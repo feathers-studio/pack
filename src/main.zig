fe80ada1fd0c1da00afa6a1b57d4aed1cbdb7983
Thomas Rory Gummerson <thomas@gummerson.no>
2023-02-13 12:55:56 +0100

    Don't add nonexistant paths to pack file, cleanup

--- src/main.zig
+++ src/main.zig
@@ -11,57 +11,67 @@ const Pack = struct {
             return std.meta.stringToEnum(Command, str) orelse .help;
         }
     };
-    fn help(out: anytype) !void {
+    const Writer = std.fs.File.Writer;
+    fn help(out: Writer) !u8 {
         try out.writeAll(
             \\Help text
             \\
         );
+        return 1;
     }
-    fn init(out: anytype) !void {
-        const file = std.fs.cwd().createFile(".pack", .{ .exclusive = true }) catch |err| {
-            switch (err) {
-                error.PathAlreadyExists => {
-                    try out.writeAll(".pack file already exists in current directory. Skipping.\n");
-                    return;
-                },
-                else => {
-                    try out.writeAll("Surprising.\n");
-                    return err;
-                },
-            }
+    fn init(cwd: std.fs.Dir, out: Writer) !void {
+        const file = cwd.createFile(".pack", .{ .exclusive = true }) catch |err| {
+            if (err == error.PathAlreadyExists)
+                try out.writeAll(".pack file already exists in current directory. Skipping.\n");
+            return err;
         };
         defer file.close();
         try out.writeAll("Created empty .pack file in current directory.\n");
     }
-    fn add(files: []const []const u8, out: anytype) !u8 {
+    fn add(cwd: std.fs.Dir, files: []const []const u8, out: Writer) !u8 {
         if (files.len == 0) {
             try out.print("Pass at least one file to add.\n", .{});
             return 2;
         }
-        const file = try std.fs.cwd().openFile(".pack", .{ .mode = .read_write });
+        const file = cwd.openFile(".pack", .{ .mode = .read_write }) catch |err| {
+            if (err == error.FileNotFound) {
+                try out.writeAll(".pack file not found. Run `pack init` first.\n");
+                return 1;
+            }
+            return err;
+        };
         defer file.close();
 
+        var buf: [256]u8 = undefined;
+
+        // TODO: don't allow duplicates in pack file
         for (files) |f| {
+            const absolute_path = cwd.realpath(f, &buf) catch |err| {
+                if (err == error.FileNotFound) {
+                    try out.print(" - {s} doesn't exist. Skipping.\n", .{f});
+                    continue;
+                }
+                return err;
+            };
             try file.seekFromEnd(0);
-            try file.writeAll(f);
+            try file.writeAll(absolute_path);
             try file.writeAll("\n");
         }
 
         return 0;
     }
-    fn pack(allocator: std.mem.Allocator) !void {
-        const file = try std.fs.cwd().openFile(".pack", .{ .mode = .read_write });
+    fn pack(cwd: std.fs.Dir, out: Writer) !void {
+        const file = try cwd.openFile(".pack", .{ .mode = .read_only });
         defer file.close();
         // try to read config
         const reader = file.reader();
 
-        std.debug.print("Packing:\n", .{});
-        while (try reader.readUntilDelimiterOrEofAlloc(allocator, '\n', 4096)) |line| {
-            defer allocator.free(line);
+        var buf: [4096]u8 = undefined;
+        while (try reader.readUntilDelimiterOrEof(&buf, '\n')) |line| {
             // TODO(mkr): actually copy files
-            std.debug.print("- {s}\n", .{line});
+            try out.print("- {s}\n", .{line});
         }
-        std.debug.print("\nDone! You should probably commit changes.\n\n", .{});
+        try out.writeAll("\nDone! You should probably commit changes.\n\n");
     }
 };
 
@@ -76,10 +86,10 @@ pub fn main() !u8 {
     defer std.process.argsFree(allocator, args);
 
     switch (Pack.Command.from(if (args.len >= 2) args[1] else "help")) {
-        .help => try Pack.help(err),
-        .init => try Pack.init(err),
-        .add => return Pack.add(args[2..], err),
-        .pack => try Pack.pack(allocator),
+        .help => return try Pack.help(err),
+        .init => try Pack.init(std.fs.cwd(), err),
+        .add => return try Pack.add(std.fs.cwd(), args[2..], err),
+        .pack => try Pack.pack(std.fs.cwd(), err),
         .unpack => unreachable, // TODO
     }
     return 0;
